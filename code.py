f1d9660b9e77b1ec83276c81282ff8eaaccbd213
Paul Cohn <pscohn@gmail.com>
2015-01-26 23:20:21 -0500

    Expand to allow any subdirectory of Code

--- code.py
+++ code.py
@@ -3,8 +3,29 @@
 Shortcuts for moving around in bash, called from .bashrc.
 """
 import sys
+import os.path
 
-if sys.argv[1] == '':
-    print('/Users/paulcohn/Code/')
-elif sys.argv[1] == 'p':
-    print('/Users/paulcohn/Code/python')
+def main():
+    base = os.path.expanduser('~/')
+
+    d = {
+        'p': 'Code/python/',
+        'c': 'Code/c',
+        'd': 'Code/django/',
+        'cb': 'Code/django/commonplace/',
+        'h': 'Code/haskell/', 
+    }
+
+    add = ''
+    if len(sys.argv) > 1:
+        add = '/'.join(sys.argv[2:])
+    else:
+        print(base + 'Code/')
+        return
+
+    if sys.argv[1] in d:
+        print(base + d[sys.argv[1]] + add)
+    else:
+        print(base + 'Code/' + sys.argv[1])
+
+sys.exit(main())
