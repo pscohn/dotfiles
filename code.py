168f08025c799ceb194b216f26f614991cac1f12
Paul Cohn <pscohn@gmail.com>
2015-02-18 16:43:09 -0500

    Make code folder lowercase

--- code.py
+++ code.py
@@ -9,25 +9,25 @@ def main():
     base = os.path.expanduser('~/')
 
     d = {
-        'p': 'Code/python/',
-        'c': 'Code/c',
-        'd': 'Code/django/',
-        'cb': 'Code/django/commonplace/',
-        'h': 'Code/haskell/', 
-        'j': 'Code/javascript/',
-        'g': 'Code/go/src/github.com/pscohn'
+        'p': 'code/python/',
+        'c': 'code/c',
+        'd': 'code/django/',
+        'cb': 'code/django/commonplace/',
+        'h': 'code/haskell/', 
+        'j': 'code/javascript/',
+        'g': 'code/go/src/github.com/pscohn'
     }
 
     add = ''
     if len(sys.argv) > 1:
         add = '/'.join(sys.argv[2:])
     else:
-        print(base + 'Code/')
+        print(base + 'code/')
         return
 
     if sys.argv[1] in d:
         print(base + d[sys.argv[1]] + add)
     else:
-        print(base + 'Code/' + sys.argv[1])
+        print(base + 'code/' + sys.argv[1])
 
 sys.exit(main())
